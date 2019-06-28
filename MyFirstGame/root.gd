70a4408e3e3a98794b1e005b31092c96495a5089
RinatB2017 <tux4096@gmail.com>
2019-06-29 01:06:14 +0300

    update

---
+++ MyFirstGame/root.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func keyboard():
+	if(Input.is_action_pressed("ui_left")):
+		$player.position.x = $player.position.x - 1
+
+	if(Input.is_action_pressed("ui_right")):
+		$player.position.x = $player.position.x + 1
+
+	if(Input.is_action_pressed("ui_up")):
+		$player.position.y = $player.position.y - 1
+
+	if(Input.is_action_pressed("ui_down")):
+		$player.position.y = $player.position.y + 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+#	keyboard()
+	$player.look_at($alien.position)

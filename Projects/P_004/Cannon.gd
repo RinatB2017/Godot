7bf1f2d595f0968204c39e5e1b7a2f28a412a806
RinatB2017 <tux4096@gmail.com>
2019-07-04 19:19:52 +0300

    update

--- Projects/P_004/Cannon.gd
+++ Projects/P_004/Cannon.gd
@@ -1,13 +1,40 @@
-extends Node2D
+extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const SPEED = 100
+var dir = Vector2()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var Bullet = preload("res://Bullet.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func push():
+	var bul = Bullet.instance()
+	bul.position = position + Vector2(50, 0)
+#	var imp = position - get_global_mouse_position()	
+	get_node("../").add_child(bul)
+	bul.apply_impulse(Vector2(), Vector2(800, -100))
+
+func _physics_process(delta):
+	# mouse push
+	if Input.is_action_just_pressed("push"):
+		push()
+
+	if Input.is_action_pressed("ui_left"):
+		dir.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		dir.x = 1
+	else:
+		dir.x = 0
+
+	if Input.is_action_pressed("ui_up"):
+		dir.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		dir.y = 1
+	else:
+		dir.y = 0
+
+	if dir.x:
+		dir.x *= SPEED
+		move_and_slide(dir)
+
+	if dir.y:
+		dir.y *= SPEED
+		move_and_slide(dir)

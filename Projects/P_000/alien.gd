b082cdfb092975c2f30c5ff45aca03d751b3ef4e
RinatB2017 <tux4096@gmail.com>
2019-07-05 23:13:38 +0300

    update

---
+++ Projects/P_000/alien.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var speed = Vector2(50, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func keyboard():
+	if(Input.is_action_pressed("ui_left")):
+		$player.position.x = $player.position.x - 1
+
+	if(Input.is_action_pressed("ui_right")):
+		$player.position.x = $player.position.x + 1
+
+	if(Input.is_action_pressed("ui_up")):
+		$player.position.y = $player.position.y - 1
+
+	if(Input.is_action_pressed("ui_down")):
+		$player.position.y = $player.position.y + 1
+		
+func player_moved(delta):
+	$player.look_at($alien.position)
+	$player.position += speed.rotated($player.rotation) * delta
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	keyboard()
+	player_moved(delta)
--- Projects/P_002/Platform_100.gd
+++
@@ -1,5 +0,0 @@
-extends StaticBody2D
-
-func _physics_process(delta):
-	if $"../Player".flag_on_floor:
-		print("OK")

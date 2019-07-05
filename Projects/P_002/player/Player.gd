6e48e2c32001d939cea57e157bfed489f0ac1e74
RinatB2017 <tux4096@gmail.com>
2019-07-05 23:45:08 +0300

    update

--- Projects/P_002/player/Player.gd
+++ Projects/P_002/player/Player.gd
@@ -5,10 +5,22 @@ const GRAVITY = 10
 const JUMP = 400
 const VTOP = Vector2(0, -1)
 
+var Bullet = preload("res://bullet/Bullet.tscn" )
+
 var vel = Vector2()
 var flag_on_floor = false
 
+func push():
+	var bul = Bullet.instance()
+	get_node("../").add_child(bul)
+
+	bul.position = position + Vector2(50, 0)
+	bul.apply_impulse(Vector2(), Vector2(800, 0))
+
 func _physics_process(delta):
+	if Input.is_action_just_pressed("push"):
+		push()
+	
 #	check_input()
 	if Input.is_action_pressed("ui_left"):
 		vel.x = -SPEED

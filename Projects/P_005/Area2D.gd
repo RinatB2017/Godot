7385a58e862c9d166a82da40b5d6d25ace8523e8
RinatB2017 <tux4096@gmail.com>
2019-07-08 00:57:49 +0300

    update

--- Projects/P_005/Area2D.gd
+++ Projects/P_005/Area2D.gd
@@ -1,14 +1,10 @@
 extends Area2D
 
 var angle = 0
-var time  = 0
 
 func _physics_process(delta):
-	time += delta
-	if time > 0.2:
-		time = 0
-		rotate(deg2rad(angle))
-		angle += 1
-		if angle >= 360:
-			angle = 0
-		
+	set_rotation(deg2rad(angle))
+	angle += 1
+	if angle >= 360:
+		angle = 0
+	

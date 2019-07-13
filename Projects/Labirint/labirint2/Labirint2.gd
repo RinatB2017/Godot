200705a9ed125a327d237adf8d5595f6961e85fe
RinatB2017 <tux4096@gmail.com>
2019-07-13 11:46:45 +0300

    update

--- Projects/Labirint/labirint2/Labirint2.gd
+++ Projects/Labirint/labirint2/Labirint2.gd
@@ -3,22 +3,75 @@ extends Node2D
 var velocity = Vector2()
 var speed = 500
 var timer = 0;
+var dir
+
+func move_up():
+	dir = "up"
+	velocity.x = 0
+	velocity.y = -1
+
+func move_down():
+	dir = "down"
+	velocity.x = 0
+	velocity.y = 1
+
+func move_left():
+	dir = "left"
+	velocity.x = -1
+	velocity.y = 0
+	
+func move_right():
+	dir = "right"
+	velocity.x = 1
+	velocity.y = 0
 
 func _ready():
 	randomize()
-	velocity.y = 1
+	move_down()
+	velocity = velocity.normalized() * speed
+	
+func new_dir():
+	velocity.x = rand_range(-1, 1)
+	velocity.y = rand_range(-1, 1)
 	velocity = velocity.normalized() * speed
 
+func new_dir2():
+	var random_value = randi()%4
+	if random_value == 0:	velocity.x=1
+	if random_value == 1:	velocity.x=-1
+	if random_value == 2:	velocity.y=1
+	if random_value == 3:	velocity.y=-1
+	velocity = velocity.normalized() * speed
+	
+func check_collision():
+	if dir == "up":
+		move_right()
+		velocity = velocity.normalized() * speed
+		return
+	if dir == "right":
+		move_down()
+		velocity = velocity.normalized() * speed
+		return
+	if dir == "down":
+		move_left()
+		velocity = velocity.normalized() * speed
+		return
+	if dir == "left":
+		move_up()
+		velocity = velocity.normalized() * speed
+		return
+
 func _physics_process(delta):
-	timer += delta
-	if timer > 0.1:
-		timer = 0
-		var collision = $Player.move_and_collide(velocity * delta)
-		if collision:
-			var random_value = randi()%4
-			if random_value == 0:	velocity.x=1
-			if random_value == 1:	velocity.x=-1
-			if random_value == 2:	velocity.y=1
-			if random_value == 3:	velocity.y=-1
-			velocity = velocity.normalized() * speed
-#		$Player.apply_impulse(Vector2(), Vector2(800, 0))
+#	timer += delta
+#	if timer > 0.1:
+#		timer = 0
+	var collision = $Player.move_and_collide(velocity * delta)
+	if collision:
+		check_collision()
+#			if dir == "up":		move_right()
+#			if dir == "right":	move_down()
+#			if dir == "down":	move_left()
+#			if dir == "left":	move_up()
+			#new_dir()
+			#new_dir2()
+	#$Player.apply_impulse(Vector2(), Vector2(800, 0))

352df5e921abb51b944c61b92ac2a79315f63733
RinatB2017 <tux4096@gmail.com>
2019-07-13 15:04:59 +0300

    update

---
+++ Projects/P_007/World.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+const SPEED = 200
+var vel = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		vel.x = -SPEED * delta
+	elif Input.is_action_pressed("ui_right"):
+		vel.x = SPEED * delta
+	else:
+		vel.x = 0
+		
+	if Input.is_action_pressed("ui_up"):
+		vel.y = -SPEED * delta
+	elif Input.is_action_pressed("ui_down"):
+		vel.y = SPEED * delta
+	else:
+		vel.y = 0
+		
+	$Cannon.move_and_collide(vel)
---
+++ Projects/P_007/cannon/Cannon.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+var Bullet = preload("res://bullet/Bullet.tscn")
+var timer = 0 
+const SPEED = 2000
+
+func shoot():
+	var bullet_0 = Bullet.instance()
+	var bullet_1 = Bullet.instance()
+	var bullet_2 = Bullet.instance()
+	var bullet_3 = Bullet.instance()
+	
+	get_node("../").add_child(bullet_0)
+	get_node("../").add_child(bullet_1)
+	get_node("../").add_child(bullet_2)
+	get_node("../").add_child(bullet_3)
+
+	bullet_0.position = position + Vector2(50,  0)
+	bullet_1.position = position + Vector2(0,   50)
+	bullet_2.position = position + Vector2(-50, 0)
+	bullet_3.position = position + Vector2(0,   -50)
+	
+	bullet_0.apply_impulse(Vector2(), Vector2(SPEED, 0))
+	bullet_1.apply_impulse(Vector2(), Vector2(0, SPEED))
+	bullet_2.apply_impulse(Vector2(), Vector2(-SPEED, 0))
+	bullet_3.apply_impulse(Vector2(), Vector2(0, -SPEED))
+	
+func _physics_process(delta):
+	timer += delta
+	if timer > 0.1:
+		timer = 0
+		shoot()

110ba5d6b7fc186538cc12e75be1cbd80fd00769
RinatB2017 <tux4096@gmail.com>
2019-07-18 14:10:28 +0300

    update

--- Projects/P_008/Main.gd
+++ Projects/P_008/Main.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
-		var pos = Vector2(900, 500)
-		$Obj.set_position(pos)
+		#var pos = Vector2(900, 500)
+		#$Obj.set_position(pos)
 		#$Obj.move_and_slide(pos)
-
+		var pos0 = $Sprite_0.position
+		var pos1 = $Sprite_1.position
+		var dist = pos0.distance_to(pos1)
+		print("dist " + str(dist))

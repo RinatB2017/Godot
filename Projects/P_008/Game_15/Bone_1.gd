938590e3ea82475b43b041e8a09099050a80eb30
RinatB2017 <tux4096@gmail.com>
2019-07-18 15:53:45 +0300

    update

---
+++ Projects/P_008/Game_15/Bone_1.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+# https://www.reddit.com/r/godot/comments/7xwr22/guide_how_to_click_a_sprite/
+var n_bone = "bone_1"
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click()
+
+func on_click():
+	print("Click " + str(n_bone))
---
+++ Projects/P_008/Game_15/Game_15.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+#var map = [4][4]
+
+func _ready():
+#	var n = 0
+#	for y in 3:
+#		for x in 3:
+#			map[x][y] = n
+#			n += 1
+#	print(map[2][3])
+	pass

3e6aad988d14047410284084fc57e68fda43d93b
RinatB2017 <tux4096@gmail.com>
2019-07-19 01:53:47 +0300

    update

--- Samples/godot-demo-projects/gui/drag_and_drop/drag_drop_script.gd
+++ Samples/godot-demo-projects/gui/drag_and_drop/drag_drop_script.gd
@@ -9,12 +9,15 @@ func get_drag_data(_pos):
 	cpb.rect_size = Vector2(50, 50)
 	set_drag_preview(cpb)
 	# Return color as drag data
+	print("get_drag_data")
 	return color
 
 
 func can_drop_data(_pos, data):
+	print("can_drop_data")
 	return typeof(data) == TYPE_COLOR
 
 
 func drop_data(_pos, data):
+	print("drop_data")
 	color = data
